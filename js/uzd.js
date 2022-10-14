7554693bea18fdd546b87c512b5a24d4a34a7afb
Rimallasfcx111 <113599111+Rimallasfcx111@users.noreply.github.com>
2022-10-14 10:47:57 +0300

    Loop pratesimas

---
+++ js/uzd.js
@@ -0,0 +1,23 @@
+'use strict';
+console.log('uzd.js');
+
+// 13. atspausdinti konsoleje skaicius nuo 12 iki 24 padaugintus is 3
+
+// 1. sukam cikla nuo 12 iki 24
+// 2. kiekvieno ciklo metu mes einamaja reiksme (index) padauginam is 3 ir atspausdinam
+for (let i = 12; i <= 24; i++) {
+  let padaugintas = i * 3;
+  console.log(`reiksme ${i} padauginta gaunasi ${padaugintas}`);
+}
+
+// 20. sudauginti skaiciu nuo kintamojo min iki kintamojo max
+// min 5. max 10
+// sudauginti ir aspausdinti skaiciu daugybos rezultata
+let min = 5;
+let max = 10;
+let total = 
+
+for (let i = min; i <= max; i++){
+    console.log(i);
+}
+console.log('min ===', min);
