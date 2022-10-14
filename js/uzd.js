911094401ba9dd63ccc895b9af690b8d81e7f0f6
Rimallasfcx111 <113599111+Rimallasfcx111@users.noreply.github.com>
2022-10-14 10:57:05 +0300

    baigtas

--- js/uzd.js
+++ js/uzd.js
@@ -5,19 +5,24 @@ console.log('uzd.js');
 
 // 1. sukam cikla nuo 12 iki 24
 // 2. kiekvieno ciklo metu mes einamaja reiksme (index) padauginam is 3 ir atspausdinam
-for (let i = 12; i <= 24; i++) {
-  let padaugintas = i * 3;
-  console.log(`reiksme ${i} padauginta gaunasi ${padaugintas}`);
-}
+// for (let i = 12; i <= 24; i++) {
+//   let padaugintas = i * 3;
+//   console.log(`reiksme ${i} padauginta gaunasi ${padaugintas}`);
+// }
 
-// 20. sudauginti skaiciu nuo kintamojo min iki kintamojo max
-// min 5. max 10
+// 20. sudauginti skaiciu nuo kintamojo total iki kintamojo max
+// total 5. max 10
 // sudauginti ir aspausdinti skaiciu daugybos rezultata
 let min = 5;
 let max = 10;
-let total = 
+// let total = 0; kai +
+let total = 1; //kai * 
 
-for (let i = min; i <= max; i++){
+for (let i = total; i <= max; i++){
     console.log(i);
+    // total = total + i;
+    total = total * i;
+    console.log('total ===', total);
+    
 }
-console.log('min ===', min);
+console.log('total ===', total); //Baigta
